2cc1d26d1cf8e731c86f3bef8c1eb1f06d63144f
Aaron Jones <aaron.jones@chandleraz.gov>
2015-10-28 09:19:34 -0700

    Initial Commit

---
+++ Vagrantfile
@@ -0,0 +1,36 @@
+# Require YAML module
+require 'yaml'
+# Read YAML file with box details
+configuration = YAML.load_file('config.yaml')
+Vagrant.require_version '>= 1.6.0'
+
+Vagrant.configure(2) do |config|
+
+	config.ssh.forward_x11 = configuration["x11forward"]
+    config.vm.box = configuration["box"]
+    config.vm.hostname = configuration["hostname"]
+    config.vm.network "private_network", ip: configuration["ip"]
+
+	# Configuration of the hardware here
+    config.vm.provider "virtualbox" do |vb|
+        vb.name = configuration["name"]
+        vb.memory = configuration["ram"]
+        vb.gui = configuration["gui"]
+        vb.cpus = configuration["cpus"]
+        vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+		# choices: hda sb16 ac97
+		vb.customize ["modifyvm", :id, '--audio', 'coreaudio', '--audiocontroller', 'hda']
+		# Set the VRAM
+		vb.customize ["modifyvm", :id, '--vram', configuration["vram"] ]
+    end
+
+	# Install the box
+    config.vm.provision :shell, :path => "vagrant/install.sh"
+    config.vm.provision :shell, :path => "vagrant/postinstall.sh"
+
+	# Any final thoughts?
+    config.vm.provision "shell", run: "always" do |s|
+		# ending message
+        #s.inline = "figlet All Done!!"
+    end
+end
---
+++ vagrant/install.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+echo "Installing!"
+if [ ! -d "/root/.ssh/" ]; then
+	mkdir /root/.ssh && touch /root/.ssh/known_hosts
+fi
+
+echo "Starting the upgrade process..."
+sudo apt-get -y update > /dev/null
+sudo apt-get -y upgrade > /dev/null
+sudo apt-get -y dist-upgrade > /dev/null
+echo "Upgrade complete!"
+
+# Install Initial Software
+echo "Installing software from the repositories..."
+sudo apt-get -y install \
+	guake \
+	newsbeuter \
+	rtorrent \
+	cmus \
+	curl \
+	git \
+	htop \
+	unzip \
+	vim; > /dev/null
+
+sudo apt-get -y install \
+	build-essential \
+	ruby \
+	cmake \
+	python-dev \
+	automake \
+	autoconf \
+	pkg-config \
+	libevent-1.4 \
+	libevent-dev \
+	libncurses5-dev \
+	autotools-dev; > /dev/null
+echo "Install complete!"
+
+# tmux v2.0 installation steps for Ubuntu 14.04 (Trusty Tahr)
+sudo apt-get install -y python-software-properties software-properties-common
+sudo add-apt-repository -y ppa:pi-rho/dev
+sudo apt-get update
+sudo apt-get install -y tmux
+tmux -V
+
+# Download fonts
+sudo git clone https://github.com/powerline/fonts.git /tmp/fonts
+
+## CPU LOAD
+sudo git clone https://github.com/thewtex/tmux-mem-cpu-load.git /tmp/tmuxcpu
+sudo cmake /tmp/tmuxcpu
+sudo make clean /tmp/tmuxcpu
+sudo make install clean /tmp/tmuxcpu
+# Plugin Manager for TMUX
+git clone https://github.com/tmux-plugins/tpm /home/vagrant/.tmux/plugins/tpm
+# Add hub to path
+sudo curl https://hub.github.com/standalone -Lo /usr/bin/hub
+sudo chmod 755 /usr/bin/hub
+
+echo "Installing dotfiles!"
+# dotfile time!
+if [ ! -d "/home/vagrant/.dotfiles" ]; then
+	git clone https://github.com/IWriteThings/dotfiles.git /home/vagrant/.dotfiles
+	sudo chown -R vagrant:vagrant /home/vagrant/.dotfiles
+	rm /home/vagrant/.bash*
+	rm /home/vagrant/.profile
+	ln -s /home/vagrant/.dotfiles/.bash* /home/vagrant/
+	ln -s /home/vagrant/.dotfiles/.tmux.* /home/vagrant/
+	ln -s /home/vagrant/.dotfiles/.dircolors /home/vagrant/
+	ln -s /home/vagrant/.dotfiles/.profile /home/vagrant/
+	ln -s /home/vagrant/.dotfiles/.vimrc /home/vagrant/
+	ln -s /home/vagrant/.dotfiles/urls /home/vagrant/.newsbeuter/
+	git clone https://github.com/VundleVim/Vundle.vim.git /home/vagrant/.vim/bundle/Vundle.vim
+	tmux source /home/vagrant/.tmux.conf
+	sudo chown -R vagrant:vagrant /home/vagrant/.vim
+	# Install Vim plugins
+	su -c "vim +PluginInstall +qall +silent" vagrant
+	# Install YCM
+	su -c "/home/vagrant/.vim/bundle/YouCompleteMe/install.py" vagrant
+fi
+
+# Install custom fonts
+sudo sh /tmp/fonts/install.sh
+
+# Remove any holds by root in vagrant home
+sudo chown -R vagrant:vagrant /home/vagrant
+
+## Autostart Guake on boot
+sudo cp /usr/share/applications/guake.desktop /etc/xdg/autostart/
